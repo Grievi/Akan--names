e034f0a6bf3bfdaf749de6e7dbcf2bef8508c6ea
Grievin <grievin.okumu@student.moringaschool.com>
2021-07-17 23:42:03 +0300

    introduced function validate

---
+++ js/index.js
@@ -0,0 +1,8 @@
+var validate = function(){
+  var maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+
+  var femaleNames = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama",];
+
+  var daysOfWeek =  ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+
+}
